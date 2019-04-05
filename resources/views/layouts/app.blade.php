fe870fa336646f8d9ebcc75981b0316d2fb1d823
chang <jkc26@njit.edu>
2019-04-05 19:47:50 -0400

    Task: Create controller route and view for about page, add to menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,10 @@
                                         Contact
 
                                     </a>
+                                    <a class="dropdown-item" href="{{ route('about') }}">
+                                        About
+
+                                    </a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/contact', 'HomeController@contact')->name('contact');
+Route::get('/about', 'HomeController@about')->name('about');
+
 
