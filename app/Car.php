25c11d97b3016c16c09d7d6bcc848721b728a4d3
chang <jkc26@njit.edu>
2019-04-05 19:42:17 -0400

    Task: Create database seed to generate 50 cars

--- app/Car.php
+++ app/Car.php
@@ -3,9 +3,12 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Car extends Model
 {
+    use Notifiable;
+
     protected $fillable = [
         'Make', 'Model', 'Year',
     ];
--- database/factories/CarFactory.php
+++ database/factories/CarFactory.php
@@ -5,5 +5,7 @@ use Faker\Generator as Faker;
 $factory->define(App\Car::class, function (Faker $faker) {
     return [
         'Make' => $faker->randomElement(['Ford', 'Honda', 'Toyota']),
+        'Model' => $faker->word,
+        'Year' => $faker->year($max = 'now'),
     ];
 });
