cd1dc0e0d3faaead5e9e30894d41f998235ba6a9
chang <jkc26@njit.edu>
2019-04-06 00:04:11 -0400

    Unit Test: Car Make

---
+++ tests/Unit/CarMakeTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class CarMakeTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::inRandomOrder()->first();
+        $array = ['Ford', 'Honda', 'Toyota'];
+        $this->assertContains( $car->Make, $array );
+    }
+}
