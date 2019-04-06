64e3460695b2b25c8201a937d20958455213f915
chang <jkc26@njit.edu>
2019-04-06 00:05:53 -0400

    Unit Test: Car Model

---
+++ tests/Unit/CarModelTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Unit;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Car;
+
+class CarModelTest extends TestCase
+{
+    /**
+     * A basic unit test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $car = Car::inRandomOrder()->first();
+        $this->assertIsString($car->Model);    }
+}
